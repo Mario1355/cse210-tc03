33f42b9dd61a992e43fb81494343a2eebfe40d60
alwaid42 <91708990+alwaid42@users.noreply.github.com>
2021-10-03 00:56:09 -0300

    Update program.py

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ from clark import add_tyyr
 from schmidt import add_schmidt
 from Elizabeth import myname
 from espinoza import add_espinoza
+from benjamin import add_ben
 
 print("Welcome to our collaborative program.")
 
@@ -9,3 +10,4 @@ add_tyyr()
 add_schmidt()
 myname()
 add_espinoza()
+add_ben()
