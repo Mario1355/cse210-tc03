141b423e4f488a064190814a74719e59a0106680
keviner99 <esp21007@byui.edu>
2021-10-02 22:51:44 -0500

    updated

--- program.py
+++ program.py
@@ -1,9 +1,11 @@
 from clark import add_tyyr
 from schmidt import add_schmidt
 from Elizabeth import myname
+from espinoza import add_espinoza
 
 print("Welcome to our collaborative program.")
 
 add_tyyr()
 add_schmidt()
 myname()
+add_espinoza()
